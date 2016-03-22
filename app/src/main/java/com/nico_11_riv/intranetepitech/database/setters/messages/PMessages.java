4f35594be4befdf680280d302f27fa9f2144dc54
Nico_11_Riv <nicolas.rvr@gmail.com>
2016-03-22 19:33:28 +0100

    case

---
+++ app/src/main/java/com/nico_11_riv/intranetepitech/database/setters/messages/PMessages.java
@@ -0,0 +1,54 @@
+package com.nico_11_riv.intranetepitech.database.setters.messages;
+
+import com.nico_11_riv.intranetepitech.database.Messages;
+import com.nico_11_riv.intranetepitech.database.setters.user.GUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class PMessages {
+    public PMessages() {
+    }
+
+    public void init(String api) {
+        GUser user = new GUser();
+        try {
+            JSONArray messages = new JSONArray(api);
+            List<Messages> m = Messages.findWithQuery(Messages.class, "SELECT * FROM Messages WHERE token = ?", user.getToken());
+            for (int n = 0; n < m.size(); n++) {
+                Messages message = m.get(n);
+                message.setOld(true);
+                message.save();
+            }
+            for (int i = 0; i < messages.length(); ++i) {
+                JSONObject tmp = messages.getJSONObject(i);
+                Messages msg;
+                m = Messages.findWithQuery(Messages.class, "SELECT * FROM Messages WHERE token = ? AND idmessage = ?", user.getToken(), tmp.getString("id"));
+                if (m.size() > 0)
+                    msg = m.get(0);
+                else
+                    msg = new Messages(user.getToken());
+                msg.setOld(false);
+                msg.setIdmessage(tmp.getString("id"));
+                msg.setTitle(tmp.getString("title"));
+                msg.setContent(tmp.getString("content"));
+                msg.setDate(tmp.getString("date"));
+                msg.setLogin(tmp.getJSONObject("user").getString("title"));
+                msg.setPicture(tmp.getJSONObject("user").getString("picture"));
+                msg.save();
+            }
+            m = Messages.findWithQuery(Messages.class, "SELECT * FROM Messages WHERE token = ?", user.getToken());
+            for (int n = 0; n < m.size(); n++) {
+                Messages message = m.get(n);
+                if (message.isOld())
+                    message.delete();
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/nico_11_riv/intranetepitech/database/setters/modules/PModules.java
@@ -0,0 +1,39 @@
+package com.nico_11_riv.intranetepitech.database.setters.modules;
+
+
+import com.nico_11_riv.intranetepitech.database.setters.user.GUser;
+import com.nico_11_riv.intranetepitech.database.Modules;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class PModules {
+    public PModules(String api) {
+        GUser user = new GUser();
+        try {
+            JSONObject obj = new JSONObject(api);
+            JSONArray json = obj.getJSONArray("modules");
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject tmp = json.getJSONObject(i);
+
+                Modules modules = new Modules(user.getToken());
+                modules.setScolaryear(tmp.getString("scolaryear"));
+                modules.setCodemodule(tmp.getString("codemodule"));
+                modules.setCodeinstance(tmp.getString("codeinstance"));
+                modules.setTitle(tmp.getString("title"));
+                modules.setDateins(tmp.getString("date_ins"));
+                modules.setCycle(tmp.getString("cycle"));
+                modules.setGrade(tmp.getString("grade"));
+                modules.setCredits(tmp.getString("credits"));
+                modules.setBarrage(tmp.getString("barrage"));
+                modules.save();
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
